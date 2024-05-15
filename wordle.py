dfd787ae9e98be2b931a879f4a22c411bc42a0e8
whelas64 <shane.whelan64@mail.dcu.ie>
2024-05-15 03:26:57 +0100

    testing version

--- wordle.py
+++ wordle.py
@@ -22,7 +22,7 @@ def Value(prev_guess="", prev_feedback=[], words = []):
             if valid:
                 new_words.append(word)
         words = new_words
-        print(f'Number of words left: {len(words)}')
+        #print(f'Number of words left: {len(words)}')
         
         freqpos_dict = {}
         for word in words:
@@ -43,8 +43,8 @@ def Value(prev_guess="", prev_feedback=[], words = []):
             word_values[word] = value
             
     words = sorted(words, key = lambda x: word_values[x], reverse = True)
-    print(f"your next guess should be: {words[0]}")
-    return words, word_values
+    #print(f"your next guess should be: {words[0]}")
+    return words
 
 YELLOW = '\033[33m'
 BRIGHT_GREEN = '\033[92m'
@@ -54,55 +54,42 @@ def wordle():
     with open("newwords.txt") as file:
         words = file.readlines()
         words = [word.strip() for word in words]
-        word = random.choice(words)
-        
-    #create a frequency dictionary for each letter in each position in words
-    freqpos_dict = {}
-    for word in words:
-        for i in range(1,5):
-            if word[i] not in freqpos_dict:
-                freqpos_dict[word[i]] = 1
-            else:
-                freqpos_dict[word[i]] += 1
-                
-    word_values = {}
-    for word in words:
-        value = 0
-        seen = []
-        for letter in word:
-            if letter not in seen:
-                value += freqpos_dict[letter]
-                seen.append(letter)
-        word_values[word] = value
-    
-    #order the words by value
-    words = sorted(words, key = lambda x: word_values[x], reverse = True)
-    
     guesses = 0
+    word = random.choice(words)
+    guess = ""
     while True:
         feedback = []
-        guess = input("Enter guess: ")
+        if not guess:
+            guess = random.choice(words)
+        else:
+            guess = words[0]
+            
         if guess == word:
-            print(f"{BRIGHT_GREEN}{word}{RESET}")
-            print(f"{BRIGHT_GREEN}You win!{RESET}")
+            #print(f"{BRIGHT_GREEN}{word}{RESET}")
+            #print(f"{BRIGHT_GREEN}You win!{RESET}")
             break
         else:
             guesses += 1
             if guesses == 6:
-                print(f"Out of guesses! The word was {YELLOW}{word}{RESET}")
+                #print(f"Out of guesses! The word was {YELLOW}{word}{RESET}")
                 break
             else:
                 for letters in range(len(word)):
                     if guess[letters] == word[letters]:
-                        print(f"{BRIGHT_GREEN}{guess[letters]}{RESET}", end="")
+                        #print(f"{BRIGHT_GREEN}{guess[letters]}{RESET}", end="")
                         feedback.append(1) # correct letter in correct position
                     elif guess[letters] in word:
-                        print(f"{YELLOW}{guess[letters]}{RESET}", end="")
+                        #print(f"{YELLOW}{guess[letters]}{RESET}", end="")
                         feedback.append(2) # correct letter in wrong position
                     else:
-                        print(guess[letters], end="")
+                        #print(guess[letters], end="")
                         feedback.append(0) # incorrect letter
-                print()
-        words, word_values = Value(guess, feedback, words)
-                        
-wordle()
+                #print()
+        words = Value(guess, feedback, words)
+    return guesses
+
+total_guesses = 0
+for i in range(2500):                
+    total_guesses += wordle()
+
+print(f"Average guesses: {total_guesses/1000}")
